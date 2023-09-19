0ac51593f464c93cbebd9bf9a93d69cb7bb5b00e
monica-otero <135083719+monica-otero@users.noreply.github.com>
2023-09-18 22:14:04 -0600

    Examen 2

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'auto',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/paperback/',
+    	url: 'paperback.html',
+    	name: 'paperback',
+  		},
+		{
+		path: '/contact/',
+    	url: 'contact.html',
+    	name: 'contact',
+  		},
+		{
+		path: '/cigarette/',
+    	url: 'cigarette.html',
+    	name: 'cigarette',
+  		},
+
+		
+	],
+	dialog: {
+		title: 'Waffle Daffle',
+		buttonOk: 'Aceptar',
+		buttoncancel: 'Cancelar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+
+
+
+
+	
+
+
+
+
+
